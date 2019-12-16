79e47083059882276f2c64d5eedce632f31930e5
Alex Yankelevich <alex@12chairs.com>
2019-12-15 23:23:23 -0500

    Added bucket names

--- ready_set_go.py
+++ ready_set_go.py
@@ -2,15 +2,15 @@
 # reserved.
 
 import boto3
-from botocore.exceptions import NoCredentialsError,ClientError
+from botocore.exceptions import NoCredentialsError, ClientError
 import sys
 
 # Before running the code, check that the ~/.aws/credentials file contains your credentials.
 
 def get_bucket_count():
-    print("============================================");
-    print("Welcome to the AWS Boto3 SDK! Ready, Set, Go!");
-    print("============================================");
+    print("============================================")
+    print("Welcome to the AWS Boto3 SDK! Ready, Set, Go!")
+    print("============================================")
 
     try:
         s3 = boto3.resource('s3')
@@ -19,9 +19,12 @@ def get_bucket_count():
         sys.exit()
 
     try:
-        no_of_buckets = len(list(s3.buckets.all()))
-        print("You have", str(no_of_buckets), "Amazon S3 buckets.")
-        return no_of_buckets
+        no_of_buckets_l = len(list(s3.buckets.all()))
+        bucket_list = list(s3.buckets.all())
+        print(bucket_list[0].name)
+        for bucket in bucket_list:
+            print(bucket.name)
+        return no_of_buckets_l
 
     except ClientError as ex:
         print(ex)
@@ -29,4 +32,5 @@ def get_bucket_count():
 
 
 if __name__ == '__main__':
-    get_bucket_count()
+    no_of_buckets = get_bucket_count()
+    print("You have", str(no_of_buckets), "Amazon S3 buckets.")
